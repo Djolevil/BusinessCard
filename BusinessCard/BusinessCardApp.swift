57336fb35cb4ab0207d9f275581cad4a5affe74a
Djordje Knezevic <starwarrior@mycity.rs>
2023-02-23 17:27:45 +0100

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Djordje Knezevic on 23.2.23..
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
